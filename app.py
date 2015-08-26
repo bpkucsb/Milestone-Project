9f704fe61af06ad2e6c5b9f39ab64b221a6a4f33
Brendan <brendan@debian>
2015-08-26 13:24:01 -0700

    show quandl

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect
 import simplejson as json
+import requests, datetime
 import pandas as pd
-import requests
+from bokeh.plotting import output_file, show
+from bokeh.charts import TimeSeries
 
 app = Flask(__name__)
 
@@ -15,13 +17,21 @@ def index():
 
 @app.route("/index_post")
 def index_post():
+  year_prev_month = (datetime.datetime.now()-datetime.timedelta(days=31)).strftime('%Y-%m')
+  
   stock_ticker = request.args.get('stock_ticker','')
-  quandl='https://www.quandl.com/api/v3/datasets/WIKI/'+stock_ticker+'.json'
+  quandl='https://www.quandl.com/api/v3/datasets/WIKI/'+stock_ticker+'.json?column_index=4&start_date='+year_prev_month+'-01'
   r = requests.get(quandl + '?api_key=_AnN7yvqekPoP7s1yPJ4')
   data = json.loads(r.text)
-  stock = pd.DataFrame[data['dataset']['data']]
-  table = pd.concat([stock[stock[0]>'2015-08-01'][0],stock[stock[0]>'2015-08-01'][4]],axis=1)
-  return stock_ticker
+  stock = pd.DataFrame(data['dataset']['data'],columns=['date','close_val'])
+
+  output_file("timeseries.html")
+  PlotData = dict(close_val=stock['close_val'],date=stock['date'].astype('datetime64'))
+  p = TimeSeries(PlotData,index='date',title=stock_ticker,ylabel='Stock Closing Value')
+
+  show(p)
+  
+  return render_template("timesseries.html")
 
 if __name__ == '__main__':
   app.debug=True
